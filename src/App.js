3b32f37bad5473a8d16bc6e26d2bcddd80cc7fff
madhuvayya <madhu.vayya@gmail.com>
2020-07-20 22:27:14 +0530

    Added Greet functional component

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import logo from './logo.svg';
 import './App.css';
 import Hello from './components/Hello';
 import { render } from '@testing-library/react';
+import Greet from './components/Greet';
 
 function App() {
   return (
     <div className="App">
       <Hello />
+      <Greet />
     </div>
   );
 }
---
+++ src/components/Greet.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Greet = () => <h1>Hello madhu</h1>
+
+export default Greet;
