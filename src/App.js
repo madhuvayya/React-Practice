82e65df1691c9b8f41293129a691a2c2ab1b2800
madhuvayya <madhu.vayya@gmail.com>
2020-07-20 22:40:29 +0530

    Added ClassComponent

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import './App.css';
 import Hello from './components/Hello';
 import { render } from '@testing-library/react';
 import Greet from './components/Greet';
+import ClassComponent from './components/ClassComponent'
 
 function App() {
   return (
     <div className="App">
-      <Hello />
-      <Greet />
+      {/* <Hello /> */}
+      {/* <Greet /> */}
+      <ClassComponent />
     </div>
   );
 }
---
+++ src/components/ClassComponent.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+
+export default class ClassComponent extends Component{
+    render(){
+        return <h1>Class Component</h1>
+    } 
+}  
